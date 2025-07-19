6818e8293681271d5fa8c6a5abf2d109042f4be6
Gosewinckel <gosewinckel.lewis@gmail.com>
2025-07-19 22:36:32 +1000

    added internal package

---
+++ internal/pokecache/cache.go
@@ -0,0 +1,20 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val []byte
+}
+
+type Cache struct {
+	entry map[string]cacheEntry
+	mu sync.Mutex
+}
+
+func newCache(interval time.Duration) Cache {
+
+}
